0ca1faf4fea53f59eb6b0572bc0d6fae39c6c95e
marmutMerahJambu <119766108+rammm2005@users.noreply.github.com>
2025-05-22 15:19:21 +0800

    Update index.ts

--- src/collections/Users/index.ts
+++ src/collections/Users/index.ts
@@ -21,6 +21,59 @@ export const Users: CollectionConfig = {
       name: 'name',
       type: 'text',
     },
+    {
+      name: 'role',
+      type: 'select',
+      required: true,
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'Staf', value: 'staf' },
+        { label: 'Civitas', value: 'civitas' },
+      ],
+      defaultValue: 'civitas',
+    },
+    {
+      name: 'permissions',
+      type: 'array',
+      fields: [
+        {
+          name: 'collection',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'permissions',
+          type: 'group',
+          fields: [
+            { name: 'create', type: 'checkbox' },
+            { name: 'admin', type: 'checkbox' },
+            { name: 'read', type: 'checkbox' },
+            { name: 'update', type: 'checkbox' },
+            { name: 'delete', type: 'checkbox' },
+            { name: 'publish', type: 'checkbox' },
+            { name: 'readVersions', type: 'checkbox' },
+            { name: 'unlock', type: 'checkbox' },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'profileImage',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'bio',
+      type: 'textarea',
+    },
+    {
+      name: 'ig',
+      type: 'text',
+    },
+    {
+      name: 'github',
+      type: 'text',
+    },
   ],
   timestamps: true,
 }
